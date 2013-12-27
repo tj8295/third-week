ac50f4639237774e308bc29c628157c24bdab78d
tj8295 <tch399@gmail.com>
2013-12-27 12:47:38 -0500

    blackjack web starting point

--- third-week/tl-blackjack-webapp/main.rb
+++ third-week/tl-blackjack-webapp/main.rb
@@ -1,7 +1,41 @@
 require 'rubygems'
 require 'sinatra'
+require 'pry'
 
 set :sessions, true
+
+helpers do
+  def calculate_total(cards)
+    55
+  end
+end
+
+get '/' do
+  erb :set_name
+end
+
+
+post '/set_name' do
+  session[:player_name] = params[:player_name]
+  redirect '/game'
+end
+
+get '/game' do
+  session[:deck] = [['2', 'H'], ['3', 'D']]
+  session[:player_cards] = []
+  session[:player_cards] << session[:deck].pop
+  erb :game
+end
+
+# get '/test' do
+#   @my_var = "Tom"
+#   erb :test
+# end
+
+  # redirect '/'
+    # "From the testing action." + params[:some].to_s
+
+
 
 get '/form' do
   erb :form
@@ -35,10 +69,10 @@ get '/home/' do
   "Welcome home! Its been a while. Ok"
 end
 
-get '/' do
-  erb :first_layout
-end
+# get '/' do
+#   erb :first_layout
+# end
 
-get '/directory1/' do
-  erb :second_layout
-end
+# get '/directory1/' do
+#   erb :second_layout
+# end
