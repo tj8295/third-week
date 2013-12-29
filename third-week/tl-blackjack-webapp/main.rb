8640f74c11d876bc74c94b239f5d6196a9089d53
tj8295 <tch399@gmail.com>
2013-12-28 21:59:24 -0500

    web blackjack update

--- third-week/tl-blackjack-webapp/main.rb
+++ third-week/tl-blackjack-webapp/main.rb
@@ -27,7 +27,7 @@ helpers do
     total
   end # end calculate_total method
 
-   def image_string(card)
+   def card_image(card)
           suit = case card[0]
                       when 'H' then 'Hearts'
                       when 'D' then 'Diamonds'
@@ -35,39 +35,61 @@ helpers do
                       when 'C' then  'Clubs'
                     end
 
-         value = case card[1]
+        value = card[1]
+        if %w(J Q K A).include?(value)
+          value = case card[1]
                         when 'J' then 'jack'
                         when 'Q' then 'queen'
                         when 'K' then  'king'
                         when 'A' then  'ace'
-                      else
-                        card[1]
                       end
+        end
 
-    "#{suit}_#{value}.jpg"
+    "<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'/>"
+
   end # end image string method
+
+  def dealer_blackjack
+
+  end
 end # end helpers
 
-get '/' do
-  erb :set_name
+before do
+  @show_hit_or_stay_buttons = true
+  @blackjack = false
+  @bust = false
+  @play_again = false
+  @dealer_turn = false
 end
 
-post '/set_name' do
+get '/' do
+  if session[:player_name]
+    redirect '/game'
+  else
+    redirect '/new_player'
+  end
+end
+
+get '/new_player' do
+  erb :new_player
+end
+
+post '/new_player' do
+  if params[:player_name].strip.empty?
+    @error = "Please input name"
+    halt erb(:new_player)
+  end
+
   session[:player_name] = params[:player_name]
-  session[:initialize_game] = true
   redirect '/game'
 end
 
 get '/game' do
 
-  if session[:initialize_game]
     suits = ['H', 'D', 'S', 'C']
     cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    session[:deck] = suits.product(cards).shuffle!
 
-    deck = suits.product(cards)
-    deck.shuffle!
-
-    session[:deck] = deck
     session[:player_cards] = []
     session[:dealer_cards] = []
 
@@ -75,11 +97,58 @@ get '/game' do
     session[:dealer_cards] << session[:deck].pop
     session[:player_cards] << session[:deck].pop
     session[:dealer_cards] << session[:deck].pop
-  end
 
 
   erb :game
 end
+
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+
+  player_total = calculate_total(session[:player_cards])
+  if player_total == 21
+    @success = "Blackjack #{session[:player_name]} wins!"
+    @show_hit_or_stay_buttons = false
+    @play_again = true
+  end
+  if player_total > 21
+    @error = "Sorry #{session[:player_name]} busted."
+    @show_hit_or_stay_buttons = false
+    @play_again = true
+  end
+  erb :game
+end
+
+post '/game/player/stay' do
+  @success = "#{session[:player_name]} has chosen to stay"
+  redirect '/game/dealer'
+end
+
+
+get '/game/dealer' do
+  @show_hit_or_stay_buttons = false
+
+  # decision tree
+  dealer_total = calculate_total(session[:dealer_cards])
+  if dealer_total == 21
+    @error = "Sorry Dealer hit blackjack dealer wins!"
+  elsif dealer_total > 21
+    @success = "Congrautlations, dealer busts, you win!"
+  elsif dealer_total >= 17
+    #dealer stays
+    redirect 'game/compare/'
+  else
+    #dealer hits
+    @show_dealer_hit_button = true
+  end
+  erb :game
+end
+
+post '/game/dealer/hit' do
+    session[:dealer_cards] << session[:deck].pop
+    redirect 'game/dealer'
+end
+
 
 # get '/test' do
 #   @my_var = "Tom"
